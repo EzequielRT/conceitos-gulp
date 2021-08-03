aaf532d6d15ccbd5c3a46d840a43bfe831a2cefc
Ezequiel Rodrigues Trindade <ezefuts@gmail.com>
2021-08-02 23:30:38 -0300

    Instalação e tasks

---
+++ conceitos-gulp/gulpfile.js
@@ -0,0 +1,18 @@
+function helloTask(callback) {
+
+    callback();
+}
+
+function javascript(callback) {
+
+    callback();
+}
+
+function css(callback) {
+
+    callback();
+}
+
+exports.default = helloTask;
+exports.js = javascript;
+exports.css = css;
