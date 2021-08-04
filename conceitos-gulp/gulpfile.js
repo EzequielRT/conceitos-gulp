d2693a0499f661577f398f580e4354b33dd31965
Ezequiel Rodrigues Trindade <ezefuts@gmail.com>
2021-08-03 23:14:56 -0300

    Otimizando Imagens

--- conceitos-gulp/gulpfile.js
+++ conceitos-gulp/gulpfile.js
@@ -2,10 +2,11 @@ const { src, dest, parallel } = require('gulp');
 const rename = require('gulp-rename');
 const minifyJS = require('gulp-uglify');
 const minifyCSS = require('gulp-uglifycss');
+const minifyIMG = require('gulp-image');
 
 function javascript() {
 
-    return src('./src/js/*.js')
+    return src('src/js/*.js')
         .pipe(minifyJS())
         .pipe(rename({extname:'.min.js'}))
         .pipe(dest('dist/js'));
@@ -14,11 +15,19 @@ function javascript() {
 
 function css() {
 
-    return src('./src/css/*.css')
+    return src('src/css/*.css')
         .pipe(minifyCSS({'maxLineLen':80, 'uglyComments': true}))
         .pipe(rename({extname:'.min.css'}))
         .pipe(dest('dist/css'));
 
 }
 
-exports.default = parallel(javascript, css);
+function optimizeImage() {
+
+    return src('src/images/*.jpg')
+        .pipe(minifyIMG())
+        .pipe(dest('dist/image'));
+
+}
+
+exports.default = parallel(javascript, css, optimizeImage);
