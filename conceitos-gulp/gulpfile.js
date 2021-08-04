bca64930e2c2f3a540ba3ed0f77ca6981040a2cd
Ezequiel Rodrigues Trindade <ezefuts@gmail.com>
2021-08-03 23:19:32 -0300

    Organizando Pipes em pastas

--- conceitos-gulp/gulpfile.js
+++ conceitos-gulp/gulpfile.js
@@ -9,7 +9,7 @@ function javascript() {
     return src('src/js/*.js')
         .pipe(minifyJS())
         .pipe(rename({extname:'.min.js'}))
-        .pipe(dest('dist/js'));
+        .pipe(dest('dist/assets/js'));
 
 }
 
@@ -18,7 +18,7 @@ function css() {
     return src('src/css/*.css')
         .pipe(minifyCSS({'maxLineLen':80, 'uglyComments': true}))
         .pipe(rename({extname:'.min.css'}))
-        .pipe(dest('dist/css'));
+        .pipe(dest('dist/assets/css'));
 
 }
 
@@ -26,7 +26,7 @@ function optimizeImage() {
 
     return src('src/images/*.jpg')
         .pipe(minifyIMG())
-        .pipe(dest('dist/image'));
+        .pipe(dest('dist/assets/image'));
 
 }
 
