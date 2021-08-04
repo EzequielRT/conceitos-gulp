08a7a5749190d27a44dc9e96898def476e6ec00b
Ezequiel Rodrigues Trindade <ezefuts@gmail.com>
2021-08-03 22:48:23 -0300

    Minificando CSS

--- conceitos-gulp/gulpfile.js
+++ conceitos-gulp/gulpfile.js
@@ -1,12 +1,24 @@
-const { src, dest } = require('gulp');
+const { src, dest, parallel } = require('gulp');
 const rename = require('gulp-rename');
 const minifyJS = require('gulp-uglify');
+const minifyCSS = require('gulp-uglifycss');
 
-exports.default = function () {
+function javascript() {
 
-    return src('./src/js/**/*.js')
+    return src('./src/js/*.js')
         .pipe(minifyJS())
         .pipe(rename({extname:'.min.js'}))
-        .pipe(dest('./dist'));
+        .pipe(dest('dist/js'));
 
 }
+
+function css() {
+
+    return src('./src/css/*.css')
+        .pipe(minifyCSS({'maxLineLen':80, 'uglyComments': true}))
+        .pipe(rename({extname:'.min.css'}))
+        .pipe(dest('dist/css'));
+
+}
+
+exports.default = parallel(javascript, css);
